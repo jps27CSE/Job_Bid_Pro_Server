83daeddba69fa0be9a5ac61750b83a9401922ac6
Jack Pritom Soren <58485174+jps27CSE@users.noreply.github.com>
2023-11-05 23:30:07 +0600

    Delete Endpoint added

--- index.js
+++ index.js
@@ -128,6 +128,13 @@ async function run() {
       res.send(result);
     });
 
+    app.delete("/delete_job/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await addJobs.deleteOne(query);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
